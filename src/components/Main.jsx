41fad8799a77f2a6ad64f961eb3ad07b2cfadf40
Roman Pavlyuchenkov <pavlyuchenkov.roma@yandex.ru>
2023-08-10 00:53:42 +0300

    портирование на реакт

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -5,9 +5,9 @@ import { api } from "../utils/Api.js";
 import Card from "./Card";
 
 function Main(props) {
-  const [userName, setUserName] = React.useState();
-  const [userDescription, setUserDescription] = React.useState();
-  const [userAvatar, setUserAvatar] = React.useState();
+  const [userName, setUserName] = React.useState("");
+  const [userDescription, setUserDescription] = React.useState("");
+  const [userAvatar, setUserAvatar] = React.useState("");
 
   React.useEffect(() => {
     api
